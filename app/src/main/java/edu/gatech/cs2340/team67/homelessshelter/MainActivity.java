224f696ffe1dca5ee451e04628fb712d2a0a9ee2
Jacob Meyers <jmeyers35@gatech.edu>
2018-02-12 14:26:28 -0500

    Added logout functionality to main activity.

--- app/src/main/java/edu/gatech/cs2340/team67/homelessshelter/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/team67/homelessshelter/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.gatech.cs2340.team67.homelessshelter;
 
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void buttonLogoutCallBack(View view) {
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+
     }
 }
