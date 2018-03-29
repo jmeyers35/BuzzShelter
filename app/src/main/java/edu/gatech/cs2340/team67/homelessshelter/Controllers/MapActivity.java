1131270d32579893a3bad23137321ae314d39e6a
jmadison145 <jmadison145@gmail.com>
2018-03-29 02:59:49 -0400

    I keep on getting an error with Illegal States if someone wants to take a look

--- app/src/main/java/edu/gatech/cs2340/team67/homelessshelter/Controllers/MapActivity.java
+++ app/src/main/java/edu/gatech/cs2340/team67/homelessshelter/Controllers/MapActivity.java
@@ -1,9 +1,28 @@
 package edu.gatech.cs2340.team67.homelessshelter.Controllers;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.SearchView;
+import android.widget.TextView;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,6 +36,7 @@ import edu.gatech.cs2340.team67.homelessshelter.Models.Shelter;
 import edu.gatech.cs2340.team67.homelessshelter.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -26,7 +46,9 @@ import java.util.ArrayList;
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
+    private boolean mTwoPane;
     private static final double EARTHRADIUS = 6366198;
+    private final MapActivity.SimpleItemRecyclerViewAdapter mAdapter = new SimpleItemRecyclerViewAdapter(Model.getInstance().getShelters());
 
 
     @Override
@@ -40,6 +62,56 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
+        //Tool Bar Functionality
+        SearchView searchView = (SearchView) findViewById(R.id.searchview_map);
+        // listening to search query text change
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // filter recycler view when query submitted
+                mAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+                mAdapter.getFilter().filter(query);
+                return false;
+            }
+        });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_map);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (findViewById(R.id.shelter_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+        View recyclerView = findViewById(R.id.shelter_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+
     }
 
 
@@ -52,8 +124,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         LatLng Atlanta = new LatLng(33.74, -84.38);
 
         map.addMarker(new MarkerOptions().position(Atlanta).title("Atlanta"));
-
-        //TODO: Add Boundary Box or initial zoom area
 
         //Builds the Boundary Box
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
@@ -125,5 +195,146 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         return Math.toDegrees(rad);
     }
 
+
+
+    //Filtering
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> implements Filterable {
+
+        private final List<Shelter> mValues;
+        private List<Shelter> mValuesFiltered;
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Shelter item = (Shelter) view.getTag();
+                Log.d("ACK", Integer.toString(item.getId())); //debug #todo delete this and its import
+                if (mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putString(ShelterDetailFragment.ARG_ITEM_ID, item.getName()); //Pass the name to the detail fragment
+                    ShelterDetailFragment fragment = new ShelterDetailFragment();
+                    fragment.setArguments(arguments);
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.shelter_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, ShelterDetailActivity.class);
+                    intent.putExtra(ShelterDetailFragment.ARG_ITEM_ID, item.getName());
+
+                    context.startActivity(intent);
+                }
+            }
+        };
+
+        SimpleItemRecyclerViewAdapter(List<Shelter> items) {
+            mValues = items;
+            mValuesFiltered = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.shelter_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mIdView.setText(mValuesFiltered.get(position).getName());
+            //holder.mContentView.setText(mValuesFiltered.get(position).getPhoneNumber()); //#TODO: I think more info in our list goes here
+
+            holder.itemView.setTag(mValuesFiltered.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValuesFiltered.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final TextView mIdView;
+            final TextView mContentView;
+
+            ViewHolder(View view) {
+                super(view);
+                mIdView = (TextView) view.findViewById(R.id.id_text);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+        }
+
+        @Override
+        public Filter getFilter() {
+            return new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence charSequence) {
+                    String charString = charSequence.toString();
+                    if (charString.isEmpty()) {
+                        mValuesFiltered = mValues;
+                    } else {
+                        List<Shelter> filteredList = new ArrayList<>();
+                        for (Shelter row : mValues) {
+
+                            // match conditions
+                            // here we are looking for name or phone number match
+                            //#TODO: add better filtering options
+                            if (row.getName().toLowerCase().contains(charString.toLowerCase()) ||
+                                    row.getRestrictions().toLowerCase().contains(charString.toLowerCase())) {
+                                if (charString.toLowerCase().contentEquals("men")) {
+                                    if(!row.getRestrictions().toLowerCase().contains("women") ||
+                                            row.getRestrictions().toLowerCase().contains("anyone")) {
+                                        filteredList.add(row); //#TODO need a better way, but this kinda handles weird 'men" characters in 'women' case
+                                    }
+
+                                } else if (charString.toLowerCase().contentEquals("male")) {
+                                    if(!row.getRestrictions().toLowerCase().contains("female") ||
+                                            row.getRestrictions().toLowerCase().contains("anyone")) {
+                                        filteredList.add(row);//#TODO need a better way, but this kinda handles weird 'men" characters in 'women' case
+                                    }
+                                } else {
+                                    filteredList.add(row);
+                                }
+                            }
+
+                        }
+
+                        mValuesFiltered = filteredList;
+                    }
+
+                    FilterResults filterResults = new FilterResults();
+                    filterResults.values = mValuesFiltered;
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                    mValuesFiltered = (ArrayList<Shelter>) filterResults.values;
+                    notifyDataSetChanged();
+                }
+            };
+        }
+
+    }
 
 }
